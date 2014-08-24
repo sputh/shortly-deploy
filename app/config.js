ccd56b88f7cf6b5fa087b5cf6eb2684ba47e5efb
Hack Reactor Students <accounts@hackreactor.com>
2014-08-23 17:13:33 -0700

    Added database URL

--- app/config.js
+++ app/config.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var host = process.env.HOST || '127.0.0.1';
+var host = process.env.HOST || 'mongodb://MongoLab-v2:db5lARkfXIfYoYru2AW0BdZog8CG2uq8vk78bCiZZVQ-@ds050077.mongolab.com:50077/MongoLab-v2'|| '127.0.0.1';
 var path = require('path');
 
 mongoose.connect(host);
